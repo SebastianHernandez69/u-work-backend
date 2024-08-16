d14f98daa0d6f02b3cabaadadd678bc597bac1bd
Daniel <danyochoa112@gmail.com>
2024-08-16 00:18:10 -0600

    Servicio para crear una solicitud desde la empresa o el solicitante

--- src/main/java/com/example/proyecto_analisis/controllers/SolicitudesController.java
+++ src/main/java/com/example/proyecto_analisis/controllers/SolicitudesController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.proyecto_analisis.models.dto.SolicitudDTO;
 import com.example.proyecto_analisis.services.SolicitudesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -20,9 +23,19 @@ public class SolicitudesController {
     SolicitudesService solicitudesService;
     
     @PutMapping("/actualizar")
-    public ResponseEntity<String> putMethodName(@RequestParam("idSolicitud") int idSolicitud, @RequestParam("idEstadoSolicitud") int idEstadoSolicitud) {
+    public ResponseEntity<String> cambiarEstadoSolicitud(@RequestParam("idSolicitud") int idSolicitud, @RequestParam("idEstadoSolicitud") int idEstadoSolicitud) {
         try {
             solicitudesService.cambiarEstadoSolicitud(idSolicitud, idEstadoSolicitud);
+            return ResponseEntity.ok("Se ha cambiado el estado de la solicitud correctamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al actualizar: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> crearSolicitud(@RequestBody SolicitudDTO solicitud) {
+        try {
+            solicitudesService.crearSolicitud(solicitud);
             return ResponseEntity.ok("Se ha cambiado el estado de la solicitud correctamente");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al actualizar: " + e.getMessage());
--- src/main/java/com/example/proyecto_analisis/services/SolicitudesService.java
+++ src/main/java/com/example/proyecto_analisis/services/SolicitudesService.java
@@ -3,6 +3,7 @@ package com.example.proyecto_analisis.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.proyecto_analisis.models.dto.SolicitudDTO;
 import com.example.proyecto_analisis.repository.SolicitudesRepository;
 
 @Service
@@ -13,6 +14,16 @@ public class SolicitudesService {
 
     public void cambiarEstadoSolicitud(int idSolicitud, int idEstadoSolicitud){
         solicitudesRepository.actualizarEstadoSolicitud(idSolicitud, idEstadoSolicitud);
+    }
+
+    public void crearSolicitud(SolicitudDTO solicitud){
+
+        solicitudesRepository
+        .crearSolicitud(solicitud.getIdOferta(),
+                        solicitud.getIdSolicitante(), 
+                        solicitud.getIdEstadoSolicitud(),
+                        solicitud.getEmisorSolicitud(),
+                        solicitud.getDescripcion());
     }
 
 }
