1430763998ca3f4f7f2a2598d9d4de7de5f086bc
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-08-18 11:17:05 -0600

    servicio para cambiar el estado de una notificacion de la empresa

--- src/main/java/com/example/proyecto_analisis/repository/NotificacionEmpresaRepository.java
+++ src/main/java/com/example/proyecto_analisis/repository/NotificacionEmpresaRepository.java
@@ -1,8 +1,10 @@
 package com.example.proyecto_analisis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.proyecto_analisis.models.Empresa;
 
@@ -11,7 +13,7 @@ public interface NotificacionEmpresaRepository extends JpaRepository<Empresa,Int
     @Query(value = "SELECT ne.ID_NOTIFICACION_EMP,"+
                     "        ne.TITULO,"+
                     "        em.NOMBRE_EMPRESA,"+
-                    "        em.URL_LOGO,"+
+                    "        em.URL_LOGO,"+ 
                     "        ne.DESCRIPCION,"+
                     "        ne.FECHA,"+
                     "        ne.ID_SOLICITUD "+
@@ -19,5 +21,13 @@ public interface NotificacionEmpresaRepository extends JpaRepository<Empresa,Int
                     "INNER JOIN empresa em on ne.ID_EMPRESA = em.ID_EMPRESA "+
                     "WHERE ne.ID_NOTIFICACION_EMP = :idNotificacionP;", nativeQuery = true)
     public Object[] obtenerNotificacionEmpresa(@Param("idNotificacionP") int idNotificacionP);
+
+    // Cambiar estado de visualizacion a notificacion
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE `notificaciones_empresas` "+
+                   "SET ESTADO_VISUALIZACION = 1 "+
+                   "WHERE ID_NOTIFICACION_EMP = :idNotificacionEmpresa", nativeQuery = true)
+    public void cambiarEstadoNotiEmpresa(@Param("idNotificacionEmpresa") int idNotifSolicitanteP);
 
 }
